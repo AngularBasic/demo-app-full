f538c0a3158b8ecce1049c3410eec240c6d4889c
Mike Boddin <github@autorisiert.net>
2018-05-15 21:15:55 +0200

    add full app

---
+++ src/app/customers/customer-details/customer-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-details',
+  templateUrl: './customer-details.component.html',
+  styleUrls: ['./customer-details.component.scss']
+})
+export class CustomerDetailsComponent implements OnInit {
+
+  @Input() customer: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
